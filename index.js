07075e31f9f12ded398abb1d7b971096f2315a99
MeatPieYan <robinyzg@hotmail.com>
2018-06-15 17:43:35 +0800

    add funSeePath

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const autoBind = require('./dist/autoBind');
 const reactAutoBind = require('./dist/reactAutoBind');
 const selector = require('./dist/selector');
+const funSeePath = require('./dist/path');
 
 module.exports = {
   autoBind,
   reactAutoBind,
-  selector
+  selector,
+  funSeePath
 };
